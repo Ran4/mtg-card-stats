d05b90a2f99874a8ed4ae815fb1c4e19e9700438
Rasmus Ansin <rasmus.ansin@gmail.com>
2019-09-07 09:20:25 +0200

    Add missing categories Sorceries. Add print_random_hand function

--- cardstats.py
+++ cardstats.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
+from typing import List
 from collections import Counter
+import random
 import re
 import sys
 
+def print_random_hand(deck: List[str]) -> None:
+    random.shuffle(deck)
+    for i, card_name in enumerate(deck[:18]):
+        if i < 7:
+            turn_name = f" H:"
+        else:
+            turn_name = f"T{i-7+1}:"
+        print(f"{turn_name} {card_name}")
 
 def _update_section(current_section: str, line: str) -> str:
     for section_name in ["Creatures", "Planeswalkers", "Instants",
-            "Enchantments", "Artifacts", "Equipment", "Lands"]:
+            "Sorceries", "Enchantments", "Artifacts", "Equipment", "Lands"]:
         if section_name.lower() + ":" == line.lower():
             return section_name
     return current_section
     
 def _looks_like_comment(s: str) -> bool:
-    return s.startswith("#") or s.startswith("/")
+    return s.strip().startswith("#")
 
 def print_numbers(filename: str) -> None:
     """
@@ -30,22 +40,28 @@ Instants:
         Draw two cards. You may discard a card to deal 4 dmg to target creature
     3x Ral's Outburst 2UR
         Deal 3 dmg to any target. Draw two cards, discard one
+        
+Sorceries:
+    3x Lava Coil 1R
+        Deal 4 dmg to target creature. If that creature would die, exile it instead.
+
 
 Enchantments:
     4x Quicksilver Dagger 1UR
         Enchanted creature has "{T}: This creature deals 1 dmg to target player.
-    2x Magefire Wings UR
-        Enchanted creature gets +2/+0 and has flying
+    # 2x Magefire Wings UR
+    #     Enchanted creature gets +2/+0 and has flying
 
 Artifacts:
 Equipment:
 Lands:
-    /* 10x Plains */
+    10x Plains
     10x Island
-    4x Scrylands
+    # 4x Scrylands
     """
     section: str = "other"
     counter: Counter = Counter()
+    deck: List[str] = []
     
     with open(filename) as f:
         lines = [x.strip() for x in f.read().split("\n")]
@@ -64,6 +80,7 @@ Lands:
             card_name: str = matches.groups()[1].strip()
             print(f"\t{amount}x {card_name}")
             counter[section] += amount
+            [deck.append(card_name) for _ in range(amount)]
         else:
             pass
             #~ print(stripped_line)
@@ -78,6 +95,8 @@ Lands:
     for section_name, count in counter.items():
         percent = 100 * count / total_count
         print(f"\t{count} {section_name} ({percent:.0f}%)")
+        
+    print_random_hand(deck)
         
             
 if __name__ == "__main__":
